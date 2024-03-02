5c5b3e524b238632dd028c3642ba806a185f28ba
AlbertoPRL <67210580+AlbertoPRL@users.noreply.github.com>
2024-03-01 20:43:28 -0500

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
+import { CounterContainer } from "@/Components/CounterContainer";
 
 export default function Home() {
   return (
-    <>      
+    <>  
+      <CounterContainer></CounterContainer>    
     </>
   );
 }
